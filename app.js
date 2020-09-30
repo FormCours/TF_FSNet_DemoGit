7ed18fd717747a5f4e6c52e2638aa3d05c8b2fe0
Aurélien <formation.net@outlook.be>
2020-09-30 15:17:18 +0200

    Anniversary calcul

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const {toChristmas} = require('./modules/calc-day');
+const {toChristmas, toAnniversary} = require('./modules/calc-day');
 
 console.log('Exercice 1 - Module');
 
 const nbChristmas = toChristmas();
 console.log(`Nombre de jour avant noel : ${nbChristmas}`);
+
+
+const nbAnnersary = toAnniversary(27,12);
+console.log(`Nombre de jour avant le 27/12 : ${nbAnnersary}`);
--- modules/calc-day.js
+++ modules/calc-day.js
@@ -11,12 +11,24 @@ const calcDay = {
             christmas.setFullYear(currentYear + 1);
         }
 
-
         const diff = christmas.getTime() - today.getTime();
 
         return Math.ceil(diff / ONE_DAY);
-    }
+    }, 
 
+    toAnniversary: (date, month) => {
+        const today = new Date();
+        const currentYear = today.getFullYear();
+        const anniversary = new Date(currentYear, month - 1, date);
+
+        if(today > anniversary) {
+            anniversary.setFullYear(currentYear + 1);
+        }
+
+        const diff = anniversary.getTime() - today.getTime();
+
+        return Math.ceil(diff / ONE_DAY);
+    }
 
 }
 
